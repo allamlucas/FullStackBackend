3c606ab884a688b0d55375039b2e6c7b5ff1e873
Lucas Allamandri <lucas.allamandri@vstudios.local>
2024-12-11 15:53:13 -0300

    TerceraParte

--- models/person.js
+++ models/person.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI; 
+const url = process.env.MONGODB_URI
 
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
   .then(() => {
-    console.log('Connected to MongoDB');  
+    console.log('Connected to MongoDB')
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
+    console.error('Error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name:{
@@ -24,20 +24,21 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function (v) {
-        return /^\d{2,3}-\d{5,}$/.test(v);
+        return /^\d{2,3}-\d{5,}$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     },
     required: [true, 'Phone number is required']
   }
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-});
+})
 
-module.exports = mongoose.model('personas', personSchema);
+module.exports = mongoose.model('personas', personSchema)
+mongoose.model('personas', personSchema)
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -16,8 +16,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
+  name: String,
+  number: Number,
 })
 
 const Person = mongoose.model('persona', personSchema)
@@ -25,20 +25,20 @@ const Person = mongoose.model('persona', personSchema)
 mongoose.model('persona2', personSchema)
 
 if (nombre && telefono) {
-    const persona = new Person({
-        name: nombre,
-        number: telefono,
-    })
+  const persona = new Person({
+    name: nombre,
+    number: telefono,
+  })
 
-    persona.save().then(result => {
-        console.log(`added ${nombre} number ${telefono} to phonebook`)
-        mongoose.connection.close()
-    })
+  persona.save().then(() => {
+    console.log(`added ${nombre} number ${telefono} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-    Person.find({}).then(result => {
-        result.forEach(persona => {
-            console.log(persona)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    result.forEach(persona => {
+      console.log(persona)
     })
+    mongoose.connection.close()
+  })
 }
